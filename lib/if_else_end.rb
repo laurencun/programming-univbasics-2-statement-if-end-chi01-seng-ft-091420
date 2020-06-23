62f2ebed46d92d8520e9740ac5c92d7c0f26f689
Lauren Cunningham <lauren.busbee@gmail.com>
2020-06-23 01:36:23 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -1,8 +1,5 @@
 # Write your solution here
-run_code_inside = false
-puts "Code before if...end"
-
-if run_code_inside
-  puts "code inside"
-end
-puts "Code after if...end"
+chance_of_rain = 0.2 
+ puts "Let's go outside!"
+ if chance_of_rain > 0.5
+   puts "Pack an u"
